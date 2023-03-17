524afe632e55c5eebaf3588159b86c16aa67dab2
JakubPoznański <jqbpoz02@gmail.com>
2023-03-17 12:57:15 +0100

    kata14

---
+++ kata15 Roman Numerals Encoder 6kyu/script.js
@@ -0,0 +1,31 @@
+//https://www.codewars.com/kata/51b62bf6a9c58071c600001b
+function solution(number) {
+  const dataArr = [
+    [1000, "M"],
+    [900, "CM"],
+    [500, "D"],
+    [400, "CD"],
+    [100, "C"],
+    [90, "XC"],
+    [50, "L"],
+    [40, "XL"],
+    [10, "X"],
+    [9, "IX"],
+    [5, "V"],
+    [4, "IV"],
+    [1, "I"],
+  ];
+  const FinalArr = [];
+  let value = number;
+  while (value !== 0) {
+    console.log(value);
+    for (let i = 0; i < dataArr.length; i++) {
+      if (dataArr[i][0] <= value) {
+        value = value - dataArr[i][0];
+        FinalArr.push(dataArr[i][1]);
+        break;
+      }
+    }
+  }
+  return FinalArr.join("");
+}
