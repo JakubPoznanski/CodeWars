64b070c5d8a298bbc9aa4e01a175b0ecc3541181
Jakub Laptop <jqbpoz02@gmail.com>
2023-03-30 16:43:11 +0200

    make checkAround

--- kata28 Battleship field validator 3kyu/script.js
+++ kata28 Battleship field validator 3kyu/script.js
@@ -8,63 +8,64 @@ function validateBattlefield(field) {
     arr.push(0);
     arr.unshift(0);
   });
-  const width = field[1].length;
   console.log(ocean);
 
   const battleship = {
     size: 4,
     amount: 1,
-    rows: [],
-    column: [],
+    cords: [],
   };
   const cruiser = {
     size: 3,
     amount: 2,
-    rows: [],
-    column: [],
+    cords: [],
   };
   const destroyer = {
     size: 2,
     amount: 3,
-    rows: [],
-    column: [],
+    cords: [],
   };
   const submarine = {
     size: 1,
     amount: 4,
-    rows: [],
-    column: [],
+    cords: [],
   };
 
-  const checkLeftRight = function (row, column) {
-    const currentCords = [[row, column]];
-    //row is constant
+  const check = function (row, column) {
     let long = 1;
+    let position = "dot";
     let rightCounter = column + 1;
     let leftCounter = column - 1;
     let upCounter = row - 1;
     let downCounter = row + 1;
-    while (
-      long <= 4 &&
-      rightCounter >= column + 3 &&
-      leftCounter <= column - 3
-    ) {
-      if (
-        ocean[row][rightCounter] === 1 &&
-        ocean[row][rightCounter - 1] === 1
+    const currentCords = [[row, column]];
+    const checkLeftRight = function () {
+      while (
+        long <= 3 &&
+        rightCounter <= column + 3 &&
+        leftCounter >= column - 3
       ) {
-        currentCords.push([row, rightCounter]);
-        long++;
+        if (
+          ocean[row][rightCounter] === 1 &&
+          ocean[row][rightCounter - 1] === 1
+        ) {
+          currentCords.push([row, rightCounter]);
+          long++;
+        }
+        if (
+          ocean[row][leftCounter] === 1 &&
+          ocean[row][leftCounter + 1] === 1
+        ) {
+          currentCords.push([row, leftCounter]);
+          long++;
+        }
+        leftCounter--;
+        rightCounter++;
       }
-      if (ocean[row][leftCounter] === 1 && ocean[row][leftCounter + 1] === 1) {
-        currentCords.push([row, leftCounter]);
-        long++;
-      }
-      leftCounter--;
-      rightCounter++;
-    }
-    if (long === 1) {
-      while (long <= 4 && upCounter >= row - 3 && downCounter <= row + 3) {
+      long > 1 ? (position = "horizontal") : (position = "dot");
+    };
+    const checkUpDown = function () {
+      while (long <= 3 && upCounter >= row - 3 && downCounter <= row + 3) {
         console.log("updown iteration");
         if (
           ocean[upCounter][column] === 1 &&
@@ -85,21 +86,109 @@ function validateBattlefield(field) {
         upCounter--;
         downCounter++;
       }
+      long > 1 ? (position = "vertical") : (position = "dot");
+    };
+    const CheckAround = function () {
+      let returnFlag = true;
+      if (position === "horizontal") {
+        const onlyColumns = currentCords.map(([row, column]) => column);
+        console.log(onlyColumns);
+        const max = Math.max.apply(null, onlyColumns) + 1;
+        const min = Math.min.apply(null, onlyColumns) - 1;
+        currentCords.forEach(([row, column]) => {
+          if (
+            ocean[row + 1][column] === 1 ||
+            ocean[row + 1][max] === 1 ||
+            ocean[row + 1][min] === 1 ||
+            ocean[row - 1][column] === 1 ||
+            ocean[row - 1][max] === 1 ||
+            ocean[row - 1][min] === 1 ||
+            ocean[row][max] === 1 ||
+            ocean[row][min] === 1
+          ) {
+            console.log("statek obok statku");
+            returnFlag = false;
+          }
+        });
+      }
+      if (position === "vertical") {
+        const onlyRows = currentCords.map(([row, column]) => row);
+        const max = Math.max.apply(null, onlyRows) + 1;
+        const min = Math.min.apply(null, onlyRows) - 1;
+        currentCords.forEach(([row, column]) => {
+          if (
+            ocean[row][column - 1] === 1 ||
+            ocean[row][column + 1] === 1 ||
+            ocean[min][column] === 1 ||
+            ocean[min][column + 1] === 1 ||
+            ocean[min][column - 1] === 1 ||
+            ocean[max][column] === 1 ||
+            ocean[max][column - 1] === 1 ||
+            ocean[max][column + 1] === 1
+          ) {
+            console.log("statek obok statku vertical");
+            returnFlag = false;
+          }
+        });
+      }
+      if (position === "dot") {
+        if (
+          ocean[row][column + 1] === 1 ||
+          ocean[row][column - 1] === 1 ||
+          ocean[row + 1][column] === 1 ||
+          ocean[row + 1][column - 1] === 1 ||
+          ocean[row + 1][column + 1] === 1 ||
+          ocean[row - 1][column] === 1 ||
+          ocean[row - 1][column + 1] === 1 ||
+          ocean[row - 1][column - 1] === 1
+        ) {
+        }
+      }
+      if (returnFlag) return true;
+      return false;
+    };
+    checkLeftRight();
+    if (long === 1) checkUpDown();
+    if (CheckAround()) {
+      currentCords.forEach(([row, column]) => (ocean[row][column] = 0));
+      console.log(ocean);
+      switch (long) {
+        case battleship.size:
+          battleship.cords.push(currentCords);
+          battleship.amount--;
+          break;
+        case cruiser.size:
+          cruiser.cords.push(currentCords);
+          cruiser.amount--;
+          break;
+        case destroyer.size:
+          destroyer.cords.push(currentCords);
+          destroyer.amount--;
+          break;
+        case submarine.size:
+          submarine.cords.push(currentCords);
+          battleship.amount--;
+          break;
+        default:
+          break;
+      }
     }
+    console.log(CheckAround(), "Czy dookoła jest pusto ?");
     console.log(currentCords);
+    console.log(position);
     console.log("long", long);
   };
-  checkLeftRight(8, 3);
 }
+
 validateBattlefield([
-  [1, 0, 0, 0, 0, 1, 1, 0, 0, 0],
-  [1, 0, 1, 0, 0, 0, 0, 0, 1, 0],
-  [1, 0, 1, 0, 1, 1, 1, 0, 1, 0],
-  [1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
-  [0, 0, 1, 0, 1, 1, 1, 0, 0, 0],
+  [1, 1, 1, 1, 0, 1, 1, 0, 0, 0],
   [0, 0, 1, 0, 0, 0, 0, 0, 1, 0],
-  [0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 1, 0, 0, 0, 0, 1, 0, 0],
+  [0, 0, 1, 0, 1, 1, 1, 0, 1, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
+  [0, 0, 0, 0, 1, 1, 1, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
+  [0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
 ]);
